a4ef301dab1dfc5bce646bdcea9165b1f5973bc0
cristian paul castaneda <crispualcastaneda>
2020-03-03 18:10:48 +0800

    'delete-details-edit'

--- details.php
+++ details.php
@@ -6,7 +6,7 @@
     }
 
     if(isset($_SESSION['Access']) && $_SESSION['Access'] == "administrator") {
-        echo "Welcome ".$_SESSION['UserLogin'];
+        echo "Welcome ".$_SESSION['UserLogin']."<br> <br>";
     } else {
         echo header("Location: index.php");
     }
@@ -34,6 +34,22 @@
         <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+
+    <form action="delete.php" method="post">
+        <a href="index.php">< Back</a>
+        <a href="edit.php?ID=<?php echo $row['id'];?>">Edit</a>
+        <!-- When creating a delete button it must be POST to prevent deletion data on DB easily -->
+
+            <?php if($_SESSION['Access'] == 'administrator'){?>
+                <button type="submit" name="delete">Delete</button>
+            <?php } ?>
+
+            <input type="hidden" name="ID" value="<?php echo $row['id'];?>">
+    </form>
+
+    <br>
+
     <h2><?php echo $row['first_name'];?> <?php echo $row['last_name'];?></h2>
+    <p>is a <?php echo $row['gender'];?></p>
 </body>
 </html>
